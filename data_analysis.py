c0cc2652dc2b5f9da3b928710a36793f137b8ba2
yagao <949899039@qq.com>
2025-02-28 22:09:49 +0800

    增加数据分析

---
+++ data_analysis.py
@@ -0,0 +1,70 @@
+# 用于分析数据
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_style("darkgrid")
+    
+data_name = 'daily_citi_bike_trip_counts_and_weather'
+
+# 引入数据集
+data = pd.read_csv('./data/' + data_name + '.csv',
+                   parse_dates=['date'],
+                   index_col=['date'],
+                   usecols=['date',
+                            'trips',
+                            'precipitation',
+                            'snowfall',
+                            'max_t',
+                            'min_t',
+                            'average_wind_speed',
+                            'dow',
+                            'holiday',
+                            'stations_in_service',
+                            'weekday',
+                            'weekday_non_holiday',
+                            'month',
+                            'dt',
+                            'day',
+                            'year'])
+
+# 以日期为x轴，骑行次数为y轴，绘制折线图
+plt.figure(figsize=(15,5))
+sns.lineplot(data=data, x=data.index, y=data.trips)
+plt.show()
+
+# 以月份为x轴，骑行次数为y轴，绘制折线图
+data_month = data.resample('M').sum()
+plt.figure(figsize=(15,5))
+sns.lineplot(data=data_month, x=data_month.index, y=data_month.trips)
+plt.show()
+
+# 降雨量
+# plt.figure(figsize=(15,5))
+# ax1 = plt.gca()  # 主坐标轴
+# ax2 = ax1.twinx()  # 共享x轴的次坐标轴
+
+# # 绘制骑行次数（主Y轴）
+# sns.lineplot(data=data, x=data.index, y='trips', ax=ax1, label='Trips')
+# # 绘制降水量（次Y轴）
+# sns.lineplot(data=data, x=data.index, y='precipitation', ax=ax2, color='orange', label='Precipitation')
+
+# ax1.set_ylabel('Trips (次)')
+# ax2.set_ylabel('Precipitation (mm)')
+# plt.title('骑行次数与降水量时序对比')
+# plt.show()
+
+# 站点数量与骑行次数的关系
+plt.figure(figsize=(15,5))
+ax1 = plt.gca()  # 主坐标轴
+ax2 = ax1.twinx()  # 共享x轴的次坐标轴
+
+# 绘制骑行次数（主Y轴）
+sns.lineplot(data=data, x=data.index, y='trips', ax=ax1, label='Trips')
+# 绘制降水量（次Y轴）
+sns.lineplot(data=data, x=data.index, y='stations_in_service', ax=ax2, color='orange', label='Precipitation')
+
+ax1.set_ylabel('Trips (次)')
+ax2.set_ylabel('开放的站点数量')
+plt.show()
